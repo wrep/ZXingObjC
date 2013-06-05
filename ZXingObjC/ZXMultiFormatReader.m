149b3e168266b70fdbf80fedf0dc7d14d89db88e
Witold Wasilewski <vitotao@gmail.com>
2013-06-05 17:16:02 +0200

    Formatting. Minor fixes.

--- ZXingObjC/ZXMultiFormatReader.m
+++ ZXingObjC/ZXMultiFormatReader.m
@@ -40,15 +40,15 @@
 @synthesize readers;
 
 - (id)init {
-  if (self = [super init]) {
-    self.readers = [NSMutableArray array];
-  }
-
-  return self;
+    if (self = [super init]) {
+        self.readers = [NSMutableArray array];
+    }
+    
+    return self;
 }
 
 + (id)reader {
-  return [[[ZXMultiFormatReader alloc] init] autorelease];
+    return [[[ZXMultiFormatReader alloc] init] autorelease];
 }
 
 /**
@@ -57,8 +57,8 @@
  * Use setHints() followed by decodeWithState() for continuous scan applications.
  */
 - (ZXResult *)decode:(ZXBinaryBitmap *)image error:(NSError **)error {
-  self.hints = nil;
-  return [self decodeInternal:image error:error];
+    self.hints = nil;
+    return [self decodeInternal:image error:error];
 }
 
 
@@ -66,8 +66,11 @@
  * Decode an image using the hints provided. Does not honor existing state.
  */
 - (ZXResult *)decode:(ZXBinaryBitmap *)image hints:(ZXDecodeHints *)_hints error:(NSError **)error {
-  self.hints = _hints;
-  return [self decodeInternal:image error:error];
+    if(self.hints != _hints) {
+        //it's very rich setter, beware!
+        self.hints = _hints;
+    }
+    return [self decodeInternal:image error:error];
 }
 
 
@@ -76,10 +79,10 @@
  * clients will get a <b>large</b> speed increase by using this instead of decode().
  */
 - (ZXResult *)decodeWithState:(ZXBinaryBitmap *)image error:(NSError **)error {
-  if (self.readers == nil) {
-    self.hints = nil;
-  }
-  return [self decodeInternal:image error:error];
+    if (self.readers == nil) {
+        self.hints = nil;
+    }
+    return [self decodeInternal:image error:error];
 }
 
 
@@ -89,87 +92,89 @@
  * is important for performance in continuous scan clients.
  */
 - (void)setHints:(ZXDecodeHints *)_hints {
-  [hints release];
-  hints = [_hints retain];
-
-  BOOL tryHarder = hints != nil && hints.tryHarder;
-  self.readers = [NSMutableArray array];
-  if (hints != nil) {
-    BOOL addZXOneDReader = [hints containsFormat:kBarcodeFormatUPCA] ||
-      [hints containsFormat:kBarcodeFormatUPCE] ||
-      [hints containsFormat:kBarcodeFormatEan13] ||
-      [hints containsFormat:kBarcodeFormatEan8] ||
-      [hints containsFormat:kBarcodeFormatCodabar] ||
-      [hints containsFormat:kBarcodeFormatCode39] ||
-      [hints containsFormat:kBarcodeFormatCode93] ||
-      [hints containsFormat:kBarcodeFormatCode128] ||
-      [hints containsFormat:kBarcodeFormatITF] ||
-      [hints containsFormat:kBarcodeFormatRSS14] ||
-      [hints containsFormat:kBarcodeFormatRSSExpanded];
-    if (addZXOneDReader && !tryHarder) {
-      [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
+    [hints release];
+    hints = [_hints retain];
+    
+    BOOL tryHarder = hints != nil && hints.tryHarder;
+    self.readers = [NSMutableArray array];
+    if (hints != nil) {
+        BOOL addZXOneDReader = [hints containsFormat:kBarcodeFormatUPCA] ||
+        [hints containsFormat:kBarcodeFormatUPCE] ||
+        [hints containsFormat:kBarcodeFormatEan13] ||
+        [hints containsFormat:kBarcodeFormatEan8] ||
+        [hints containsFormat:kBarcodeFormatCodabar] ||
+        [hints containsFormat:kBarcodeFormatCode39] ||
+        [hints containsFormat:kBarcodeFormatCode93] ||
+        [hints containsFormat:kBarcodeFormatCode128] ||
+        [hints containsFormat:kBarcodeFormatITF] ||
+        [hints containsFormat:kBarcodeFormatRSS14] ||
+        [hints containsFormat:kBarcodeFormatRSSExpanded];
+        if (addZXOneDReader && !tryHarder) {
+            [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
+        }
+        if ([hints containsFormat:kBarcodeFormatQRCode]) {
+            [self.readers addObject:[[[ZXQRCodeReader alloc] init] autorelease]];
+        }
+        if ([hints containsFormat:kBarcodeFormatDataMatrix]) {
+            [self.readers addObject:[[[ZXDataMatrixReader alloc] init] autorelease]];
+        }
+        if ([hints containsFormat:kBarcodeFormatAztec]) {
+            [self.readers addObject:[[[ZXAztecReader alloc] init] autorelease]];
+        }
+        if ([hints containsFormat:kBarcodeFormatPDF417]) {
+            [self.readers addObject:[[[ZXPDF417Reader alloc] init] autorelease]];
+        }
+        if ([hints containsFormat:kBarcodeFormatMaxiCode]) {
+            [self.readers addObject:[[[ZXMaxiCodeReader alloc] init] autorelease]];
+        }
+        if (addZXOneDReader && tryHarder) {
+            [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
+        }
     }
-    if ([hints containsFormat:kBarcodeFormatQRCode]) {
-      [self.readers addObject:[[[ZXQRCodeReader alloc] init] autorelease]];
+    
+    //default configuration with all readers
+    if ([self.readers count] == 0) {
+        if (!tryHarder) {
+            [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
+        }
+        [self.readers addObject:[[[ZXQRCodeReader alloc] init] autorelease]];
+        [self.readers addObject:[[[ZXDataMatrixReader alloc] init] autorelease]];
+        [self.readers addObject:[[[ZXAztecReader alloc] init] autorelease]];
+        [self.readers addObject:[[[ZXPDF417Reader alloc] init] autorelease]];
+        [self.readers addObject:[[[ZXMaxiCodeReader alloc] init] autorelease]];
+        if (tryHarder) {
+            [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
+        }
     }
-    if ([hints containsFormat:kBarcodeFormatDataMatrix]) {
-      [self.readers addObject:[[[ZXDataMatrixReader alloc] init] autorelease]];
-    }
-    if ([hints containsFormat:kBarcodeFormatAztec]) {
-      [self.readers addObject:[[[ZXAztecReader alloc] init] autorelease]];
-    }
-    if ([hints containsFormat:kBarcodeFormatPDF417]) {
-      [self.readers addObject:[[[ZXPDF417Reader alloc] init] autorelease]];
-    }
-    if ([hints containsFormat:kBarcodeFormatMaxiCode]) {
-      [self.readers addObject:[[[ZXMaxiCodeReader alloc] init] autorelease]];
-    }
-    if (addZXOneDReader && tryHarder) {
-      [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
-    }
-  }
-  if ([self.readers count] == 0) {
-    if (!tryHarder) {
-      [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
-    }
-    [self.readers addObject:[[[ZXQRCodeReader alloc] init] autorelease]];
-    [self.readers addObject:[[[ZXDataMatrixReader alloc] init] autorelease]];
-    [self.readers addObject:[[[ZXAztecReader alloc] init] autorelease]];
-    [self.readers addObject:[[[ZXPDF417Reader alloc] init] autorelease]];
-    [self.readers addObject:[[[ZXMaxiCodeReader alloc] init] autorelease]];
-    if (tryHarder) {
-      [self.readers addObject:[[[ZXMultiFormatOneDReader alloc] initWithHints:hints] autorelease]];
-    }
-  }
 }
 
 - (void)reset {
-  if (self.readers != nil) {
-    for (id<ZXReader> reader in self.readers) {
-      [reader reset];
+    if (self.readers != nil) {
+        for (id<ZXReader> reader in self.readers) {
+            [reader reset];
+        }
     }
-  }
 }
 
 - (ZXResult *)decodeInternal:(ZXBinaryBitmap *)image error:(NSError **)error {
-  if (self.readers != nil) {
-    for (id<ZXReader> reader in self.readers) {
-      ZXResult *result = [reader decode:image hints:self.hints error:nil];
-      if (result) {
-        return result;
-      }
+    if (self.readers != nil) {
+        for (id<ZXReader> reader in self.readers) {
+            ZXResult *result = [reader decode:image hints:self.hints error:nil];
+            if (result) {
+                return result;
+            }
+        }
     }
-  }
-
-  if (error) *error = NotFoundErrorInstance();
-  return nil;
+    
+    if (error) *error = NotFoundErrorInstance();
+    return nil;
 }
 
 - (void)dealloc {
-  [hints release];
-  [readers release];
-
-  [super dealloc];
+    [hints release];
+    [readers release];
+    
+    [super dealloc];
 }
 
 @end
